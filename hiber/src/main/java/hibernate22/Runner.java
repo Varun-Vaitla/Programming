a74d701a335fb907003210b71d3ac109f8e6df2a
Varun Vaitla <varunvm996@gmail.com>
2025-04-17 22:35:51 +0530

    Add files via upload

---
+++ hiber/src/main/java/hibernate22/Runner.java
@@ -0,0 +1,11 @@
+package hibernate22;
+
+public class Runner {
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.setId(12);
+		s.setName("varun");
+		s.setMarks(75.5);
+	}
+
+}
---
+++ hiber/src/main/java/hibernate22/Student.java
@@ -0,0 +1,44 @@
+package hibernate22;
+
+public class Student {
+	private int id;
+	private String name;
+	private double marks;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the marks
+	 */
+	public double getMarks() {
+		return marks;
+	}
+	/**
+	 * @param marks the marks to set
+	 */
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+
+}
